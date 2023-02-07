77e9228a1017f40a5a1461c0197d13cd032ac0ca
sunyuchen121 <1746950509@qq.com>
2023-02-07 16:47:23 +0800

    edit __getitem__ iterable test

--- ComplexKnowledgeDemo/getitem-iterable-obj-test.py
+++ ComplexKnowledgeDemo/getitem-iterable-obj-test.py
@@ -2,6 +2,10 @@ from collections.abc import Iterable, Iterator, Generator
 
 
 class GetitemIterableObj(object):
+    _index_mapping = {
+        "a": 1, "b": 2,
+        "c": 3, "d": 4
+    }
 
     def __init__(self, iterabl_obj):
         self.data_g = list(iterabl_obj)
@@ -9,6 +13,12 @@ class GetitemIterableObj(object):
     def __getitem__(self, item):
         print("item---> {}".format(item))
         return self.data_g[item]
+
+    def __getattr__(self, item):
+        if item in self._index_mapping:
+            return self.data_g[self._index_mapping[item]]
+        else:
+            return None
 
 
 inst_a = GetitemIterableObj((1, 2, 3, 4, 5))
@@ -30,3 +40,12 @@ print(param_a, *_, param_b)
 print(f"-------{isinstance(inst_a, Iterable)}-------")
 print(tuple(inst_a))
 print(dir(inst_a))
+
+print("=" * 50)
+
+# 获取实例属性的三种方式，__getattr__对第一种 inst.__dict__[key]不生效
+# print(inst_a.__dict__["a"])
+print(inst_a.a)
+print(getattr(inst_a, "a"))
+
+print(inst_a.__class__)
